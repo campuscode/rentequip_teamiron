f3d4e67e73d99c08e19d1a181bc852eafa69b334
Luiz e Renan <luizgomes.campuscode@gmail.com>
2016-05-03 22:11:14 -0300

    working on viwes all contracts

---
+++ spec/features/user_views_all_contracts_spec.rb
@@ -0,0 +1,83 @@
+require 'rails_helper'
+
+feature 'user views all contracts' do
+
+  scenario 'successfully' do
+    contract = Contract.create(responsable: 'Luiz',
+                            deadline: 5.days.from_now,
+                            client: 'MVR Engenharia',
+                            equipments: 'Furadeira, empilhadeira',
+                            amount: 1000000,
+                            delivery_address: 'Av. Paulista 9876')
+
+    contract_2 = Contract.create(responsable: 'Raul',
+                            deadline: 20.days.from_now,
+                            client: 'MVR Engenharia 2',
+                            equipments: 'Furadeira, empilhadeira, martelos',
+                            amount: 5000000,
+                            delivery_address: 'Av. Brigadeiro Luiz Antonio 2020')
+
+    contract_3 = Contract.create(responsable: 'Jonas',
+                            deadline: 30.days.from_now,
+                            client: 'MVR Engenharia 2',
+                            equipments: 'Furadeira, empilhadeira',
+                            amount: 2000000,
+                            delivery_address: 'Av. Paulista 1234')
+
+    visit contracts_path
+
+    expect(page).to have_content contract.id
+    expect(page).to have_content contract.client
+    expect(page).to have_content contract.deadline
+
+    expect(page).to have_content contract_2.id
+    expect(page).to have_content contract_2.client
+    expect(page).to have_content contract_2.deadline
+
+    expect(page).to have_content contract_3.id
+    expect(page).to have_content contract_3.client
+    expect(page).to have_content contract_3.deadline
+
+
+  end
+  scenario 'user clic on the contract' do
+    contract = Contract.create(responsable: 'Luiz',
+                            deadline: 5.days.from_now,
+                            client: 'MVR Engenharia',
+                            equipments: 'Furadeira, empilhadeira',
+                            amount: 1000000,
+                            delivery_address: 'Av. Paulista 9876')
+
+    contract_2 = Contract.create(responsable: 'Raul',
+                            deadline: 20.days.from_now,
+                            client: 'MVR Engenharia 2',
+                            equipments: 'Furadeira, empilhadeira, martelos',
+                            amount: 5000000,
+                            delivery_address: 'Av. Brigadeiro Luiz Antonio 2020')
+
+    contract_3 = Contract.create(responsable: 'Jonas',
+                            deadline: 30.days.from_now,
+                            client: 'MVR Engenharia 2',
+                            equipments: 'Furadeira, empilhadeira',
+                            amount: 2000000,
+                            delivery_address: 'Av. Paulista 1234')
+
+    visit contracts_path
+
+    expect(page).to have_content contract.id
+    expect(page).to have_content contract.client
+    expect(page).to have_content contract.deadline
+
+    expect(page).to have_content contract_2.id
+    expect(page).to have_content contract_2.client
+    expect(page).to have_content contract_2.deadline
+
+    expect(page).to have_content contract_3.id
+    expect(page).to have_content contract_3.client
+    expect(page).to have_content contract_3.deadline
+
+    within("tr:nth-child(2)") do
+      click_on "Ver detalhes"
+    end
+  end
+end
