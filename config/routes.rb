c06ffb0ce59207f837221c62f52cc2904640aa4c
Joao Almeida <joaorsalmeida@gmail.com>
2016-06-04 12:33:01 -0300

    User sign in

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  devise_for :users
   root 'contracts#index'
   resources :equipment, only: [:index]
   resources :contracts, only: [:show, :index, :new, :create] do
---
+++ spec/factories/users.rb
@@ -0,0 +1,6 @@
+FactoryGirl.define do
+  factory :user do
+    email "joao@campus.com"
+    password "123456789"
+  end
+end
